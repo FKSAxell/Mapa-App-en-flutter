e573d64ca28c1dadd5477b50fa90ca7495acf8d7
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-05-21 02:12:54 -0500

    Mi ubicación y mapas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mapa_app/bloc/mapa/mapa_bloc.dart';
+import 'package:mapa_app/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
 import 'package:mapa_app/pages/acceso_gps_page.dart';
 import 'package:mapa_app/pages/loading_page.dart';
 import 'package:mapa_app/pages/mapa_page.dart';
@@ -8,15 +11,25 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Mapa App',
-      home: LoadingPage(),
-      routes: {
-        'mapa': (_) => new MapaPage(),
-        'loading': (_) => new LoadingPage(),
-        'acceso_gps': (_) => new AccesoGpsPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => MiUbicacionBloc(),
+        ),
+        BlocProvider(
+          create: (_) => MapaBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Mapa App',
+        home: LoadingPage(),
+        routes: {
+          'mapa': (_) => new MapaPage(),
+          'loading': (_) => new LoadingPage(),
+          'acceso_gps': (_) => new AccesoGpsPage(),
+        },
+      ),
     );
   }
 }
--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mapa_app/bloc/mapa/mapa_bloc.dart';
 
-class MapaPage extends StatelessWidget {
+import 'package:mapa_app/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
+import 'package:mapa_app/widgets/widgets.dart';
+
+class MapaPage extends StatefulWidget {
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  @override
+  void initState() {
+    BlocProvider.of<MiUbicacionBloc>(context).iniciarSeguimiento();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    BlocProvider.of<MiUbicacionBloc>(context).cancelarSeguimiento();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final miUbicacionBloc = context.watch<MiUbicacionBloc>().state;
+
     return Scaffold(
-      body: Center(
-        child: Text('MapaPage'),
+      body: crearMapa(miUbicacionBloc),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          BtnUbicacion(),
+        ],
       ),
+    );
+  }
+
+  Widget crearMapa(MiUbicacionState state) {
+    if (!state.existeUbicacion) {
+      return Center(child: Text('Ubicando...'));
+    }
+    final mapaBloc = context.watch<MapaBloc>();
+    final CameraPosition cameraPosition = CameraPosition(
+      target: state.ubicacion,
+      zoom: 15,
+    );
+    return GoogleMap(
+      initialCameraPosition: cameraPosition,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
+      zoomControlsEnabled: false,
+      onMapCreated: mapaBloc.initMapa,
     );
   }
 }
